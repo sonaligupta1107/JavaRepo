c09052d767c4f02b43e66313c47311fe2cbda061
G763472 <G763472@VW10A31352.principalusa.corp.principal.com>
2023-01-31 15:37:37 +0530

    do

--- GitPro/src/Demo.java
+++ GitPro/src/Demo.java
@@ -1,9 +1,13 @@
+import pack1.MyInterfaceImpl;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("first statement");
+		
+		MyInterfaceImpl i=new MyInterfaceImpl();
+		i.disp();
+		
 	}
 
 }
---
+++ GitPro/src/pack1/MyInterfaceImpl.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class MyInterfaceImpl implements MyInterface {
+
+	@Override
+	public void disp() {
+		// TODO Auto-generated method stub
+		System.out.println("hhi");
+	}
+	
+}
